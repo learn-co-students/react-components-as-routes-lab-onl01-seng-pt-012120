79df0d1eab62a902da78b7eec4c2f3db8c85e7ea
d-otis <dan@dan-foley.com>
2020-10-01 18:58:41 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,10 +2,28 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const generateDirectors = () => {
+    return directors.map((dir, idx) => {
+      return(
+        <div>
+          <h2>Name: {dir.name}</h2>
+          <p>Movies:</p>
+          {generateTitles(dir.movies)}
+        </div>
+      )
+    })
+  }
+
+  const generateTitles = (movies) => {
+    return movies.map((title, idx) => <li key={idx}>{title}</li> )
+  }
+
   return (
-    <div>
-      <h1>Directors</h1>
-    </div>
+    <React.Fragment>
+      <h1>Directors Page</h1>
+      {generateDirectors()}
+    </React.Fragment>
   );
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const generateGenres = genres => genres.map((genre, idx) => <li key={idx}>{genre}</li>)
+
+  const generateMovies = () => {
+    return movies.map((movie, idx) => {
+      return(
+        <div key={idx}>
+          <h2>Name: {movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {generateGenres(movie.genres)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        <h1>Movies</h1>
+      <h1>Movies Page</h1>
+      {generateMovies()}
     </div>
   );
 };
