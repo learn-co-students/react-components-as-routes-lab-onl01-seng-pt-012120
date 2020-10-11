fb3e7d6a46c935640abd1fbc0f0caa907f9a6875
LeonorPDX <leonoracolbert@gmail.com>
2020-10-11 13:42:15 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+          <div>
+            <p>Title: {movie.title}</p>
+            <p>Time: {movie.time}</p>
+            Genres:
+            <ul>
+              {movie.genres.map(g => `<li>${g}</li>`)}
+            </ul>
+          </div>
+          )
+        })}
     </div>
   );
 };
