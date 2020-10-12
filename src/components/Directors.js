29864b8b1d824e41ec2a64fb69aa971d63b365ca
Lauren Dove <elledoveinc@gmail.com>
2020-10-11 23:26:39 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,33 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Directors Page</h1>
+      
+      {directors.map((director,index) => (
+          <div key={index}>
+
+      <h3> Name: {director.name}</h3>
+        <ul>
+          {directors.map((directors,index) => (
+                <li key={index}>
+                  {directors.movies}
+
+                </li>
+
+           ) )}
+          
+
+
+        </ul>
+
+
+
+
+            </div>
+
+
+
+      ))}
     </div>
   );
 }
